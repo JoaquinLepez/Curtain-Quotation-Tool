7a03be2babef9e2d444b62ee5ebdbe083c75da1f
Joaquin Lepez <joacolepez@gmail.com>
2024-07-10 21:14:55 -0300

    config(env): add .env file and update configuration references

--- app/config/config.py
+++ app/config/config.py
@@ -1,15 +1,22 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+basedir = os.path.abspath(Path(__file__).parents[2])
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config:
      SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://joaquin:123456789@localhost:5433/dev_database'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://joaquin:123456789@localhost:5433/test_database'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI')
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://joaquin:123456789@localhost:5433/database'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PROD_DATABASE_URI')
 
 config = {
     'development': DevelopmentConfig,
