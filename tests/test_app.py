6f7a07c2673d35f8a6ee641fb92db9ad7538db81
Joaquin Lepez <joacolepez@gmail.com>
2024-07-12 09:26:29 -0300

    test(app, db): Modify app and db tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,11 @@
 import unittest
+import os
 from app import create_app
 from flask import current_app
 
 class FlaskClientTestCase(unittest.TestCase):
     def setUp(self):
+        os.environ['FLASK_CONTEXT'] = 'testing'
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,12 @@
 import unittest
+import os
 from sqlalchemy import text
 from app import create_app, db
 
 class ConnectionTestCase(unittest.TestCase):
 
     def setUp(self):
+        os.environ['FLASK_CONTEXT'] = 'testing'
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
